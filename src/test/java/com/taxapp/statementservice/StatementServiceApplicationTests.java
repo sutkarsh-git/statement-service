639766c63f7b4c6dd8b57612549ccf2ac93de237
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-03-07 17:07:01 -0600

    [DEV] int

---
+++ src/test/java/com/taxapp/statementservice/StatementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxapp.statementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
