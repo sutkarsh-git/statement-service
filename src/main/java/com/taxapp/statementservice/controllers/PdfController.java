07d5ec6505ff74f726a58857aeee643cad572db7
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-03-14 01:18:33 -0600

    controller to check

--- src/main/java/com/taxapp/statementservice/controllers/PdfController.java
+++ src/main/java/com/taxapp/statementservice/controllers/PdfController.java
@@ -1,15 +1,14 @@
 package com.taxapp.statementservice.controllers;
 
 import com.taxapp.statementservice.model.PdfFile;
-import com.taxapp.statementservice.repositories.PdfRepository;
 import com.taxapp.statementservice.services.PdfService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 
@@ -21,16 +20,24 @@ public class PdfController {
         this.pdfService = pdfService;
     }
 
+    @RequestMapping({"", "/", "/index"})
+    public String home() {
+        System.out.println("Enter the home() method");
+        return "index";
+    }
+
     @GetMapping("/upload")
     public String showUploadForm(Model model) {
+        System.out.println("Inside showUploadForm method");
         model.addAttribute("pdfFile", new PdfFile());
         return "uploadform";
     }
 
     @PostMapping("/pdf-file")
-    public String handleFileUpload(@ModelAttribute("pdfFile") PdfFile pdfFile) throws IOException {
+    public String handleFileUpload(@ModelAttribute("pdfFile") PdfFile pdfFile, RedirectAttributes redirectAttributes) throws IOException {
         pdfService.savePdfFile(pdfFile);
-        return "uploadform";
+        redirectAttributes.addFlashAttribute("message", "The PDF file has been uploaded successfully.");
+        return "redirect:/upload";
     }
 
 }
